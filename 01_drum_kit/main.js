91e9dec271269d2abf339682978238ee2826edb7
asaadoov <asaadballah@gmail.com>
2018-12-30 11:06:02 +0800

    1- added drum kit (first project of JS30)

---
+++ 01_drum_kit/main.js
@@ -0,0 +1,19 @@
+function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!audio) {
+        return;
+    };
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+}
+
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', event => {
+    if (event.propertyName !== 'transform') return;
+    key.classList.remove('playing');
+}));
+
