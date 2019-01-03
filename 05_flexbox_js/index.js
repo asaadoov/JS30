dd1b84410d272af4388ca04082a0e3d1f06560e0
asaadoov <asaadballah@gmail.com>
2019-01-03 10:24:47 +0800

    3- edited style.css ( padding for the secnod child on the panel )

--- 05_flexbox_js/index.js
+++ 05_flexbox_js/index.js
@@ -1,6 +1,5 @@
 const panels = document.querySelectorAll('.panel');
 
-panels.forEach(panel => panel.classList.toggle('openActive'));
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
